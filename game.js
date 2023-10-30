32ae30ffc7554c088a1e714a53545069e56b384e
Saurabh Kumar <shikha@Saurabhs-MacBook-Air.local>
2023-10-30 19:43:52 +0530

    initial commit

---
+++ game.js
@@ -0,0 +1,17 @@
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"]
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    console.log(randomChosenColour);
+    $("#" + randomChosenColour).click(function() {
+        
+        $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+        audio.play();
+    })
+    
+}
+nextSequence()
